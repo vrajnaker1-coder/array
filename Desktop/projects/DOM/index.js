0f90125d623706622d54fd8a84ac24cae2c5bace
vrajnaker1 <vraj.naker1@gmail.com>
2025-10-21 15:53:44 +0300

    an excercise

---
+++ Desktop/projects/DOM/index.js
@@ -0,0 +1,37 @@
+// const header = document.getElementById("header")
+
+// header.textContent="tomorrow will be lit"
+
+// console.log(header.textContent)
+
+//const header= document.getElementById("head")
+//header.textContent= "This should work"
+//console.log(header.textContent)
+
+
+//const feat= document.getElementById("phrase")
+//feat.textContent= "great day"
+//console.log(feat.textContent)
+
+
+const af=document.getElementById("fort")
+af.textContent="welcome to virtual store"
+console.log(af.textContent)
+
+const js=document.getElementById("skim")
+js.textContent="2 options"
+console.log(js.textContent)
+
+const br=document.getElementById("frit")
+br.textContent="choose between basic or extreme"
+console.log(br.textContent)
+
+const feat=document.getElementById("fret")
+feat.textContent="add any extra requirements"
+console.log(feat.textContent)
+
+const frat=document.getElementById("phrases")
+frat.textContent="fill in the required details"
+console.log(frat.textContent)
+
+
